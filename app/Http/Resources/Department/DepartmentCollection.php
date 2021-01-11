532496fce820fe18b2e6f3286c56f2c7d85fccec
TikeSanOo-CKM <60506199+TikeSanOo-CKM@users.noreply.github.com>
2021-01-11 22:42:47 +0630

    start department crud

---
+++ app/Http/Resources/Department/DepartmentCollection.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources\Department;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class DepartmentCollection extends ResourceCollection
+{
+    //customize resource collection
+    public $collects = DepartmentResource::class;
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+    /**
+     * Get additional data that should be returned with the resource array.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return array
+     */
+    public function with($request)
+    {
+        return [
+            'status' => 1,
+        ];
+    }
+}
---
+++ app/Http/Resources/Department/DepartmentResource.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Resources\Department;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class DepartmentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        // return parent::toArray($request);
+        return [
+            'id'=>$this->id,
+            'name'=>$this->name,
+            'name_mm'=>$this->name_mm,
+            'description'=>$this->description
+        ];
+    }
+    /**
+     * Get additional data that should be returned with the resource array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function with($request)
+    {
+        return [
+            'status' => 1,
+        ];
+    }
+}
