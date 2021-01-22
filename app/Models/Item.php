259f4ddee431cacd824f8ea05826c26087e738cb
TikeSanOo-CKM <60506199+TikeSanOo-CKM@users.noreply.github.com>
2021-01-22 20:11:06 +0630

    item model

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -10,7 +10,7 @@ class Item extends Model
     use SoftDeletes;
     protected $casts = ['is_apply_special'=>'boolean'];
     protected $fillable = [
-        'name', 'name_mm','item_category_id','normal_price','special_price','normal_quantity','special_price','is_apply_special'
+        'item_category_id','item_sub_category_id','name', 'name_mm','normal_price','special_price','normal_quantity','special_price','is_apply_special'
     ];
      public function item_category()
     {
